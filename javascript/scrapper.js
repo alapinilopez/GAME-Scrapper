e6b5b80e8901528ac8f149c746d309b20d5335c9
alapinilopez <alapinilopez@cifpfbmoll.eu>
2022-04-06 12:17:06 +0200

    feat: :sparkles: Scrapper done

--- javascript/scrapper.js
+++ javascript/scrapper.js
@@ -1,20 +1,22 @@
-
-
-$(document).ready(function () {
-    alert("Robándole al Game");
-    
-    const webPage = fetch("https://www.game.es/buscar/juegos-ps5", {mode: "no-cors"});
-    // const webPage = "https://www.wikipedia.org/";
-    const item = {
-        name: "name",
-        prize: "price"
-        
-    };
-    $.get(webPage, function () {
-        [...$find($(".search-item:parent").children(".item-info").children('.title').children("a").each(function(i, e){console.log($(e).text())}))]
-    },
-    );
-
-});
-
-// $(".search-item:parent").children(".item-info").children('.title').text()
+$(document).ready(function (){
+    var gameTitles = []
+    $(".search-item:parent").children(".item-info").children('.title').children("a").each(function(i, e){
+      gameTitles.push($(e).text());
+      })
+      var gamePriceDecimal = []
+      $(".buy--price:parent").children(".decimal").each(function(i, e) {
+        gamePriceDecimal.push($(e).text());
+      })
+      var currency = $(".buy--price:parent").first().children(".currency").text();
+      var gamePrice = []
+      $(".buy--price:parent").children(".int").each(function(i, e) {
+        gamePrice.push($(e).text() + gamePriceDecimal[i])
+      })
+      // Ya te liabas con TypeScript he pillin....
+      var games = gameTitles.map(function(gameTitles, i) {
+        return {'name':gameTitles,
+                'price':gamePrice[i],
+                'currency': currency}
+      });
+      console.log(games)
+  });
