18577f07161bfc52454b4c4b081085273b525908
alapinilopez <alapinilopez@cifpfbmoll.eu>
2022-04-06 11:51:16 +0200

    feat: :memo: Tonto el que lo lea

--- javascript/scrapper.js
+++ javascript/scrapper.js
@@ -1,9 +1,20 @@
-(document).ready(function () {
-      $.get("https://www.game.es/buscar/juegos-ps5", function(data)
-           {[...$find($(".search-item:parent").children(".item-info").children('.title').text())]
-              alert()
-          },
-          "dataType"
-      ); 
-      
+
+
+$(document).ready(function () {
+    alert("Robándole al Game");
+    
+    const webPage = fetch("https://www.game.es/buscar/juegos-ps5", {mode: "no-cors"});
+    // const webPage = "https://www.wikipedia.org/";
+    const item = {
+        name: "name",
+        prize: "price"
+        
+    };
+    $.get(webPage, function () {
+        [...$find($(".search-item:parent").children(".item-info").children('.title').children("a").each(function(i, e){console.log($(e).text())}))]
+    },
+    );
+
 });
+
+// $(".search-item:parent").children(".item-info").children('.title').text()
