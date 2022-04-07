13fef1f62c61f6f805e0a0242e4b40cf8ff85c66
alapinilopez <alapinilopez@cifpfbmoll.eu>
2022-04-07 11:44:57 +0200

    feat: :fire: Ο Χοσέπ φυσάει γκάιντες

---
+++ betis.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    var robar = $.get("https://www.game.es/buscar/juegos-ps5").html(function() {
+    //   $("#hola").html();
+        var gameTitles = []
+        $("#hola").find(".search-item:parent").children(".item-info").children('.title').children("a").each(function (i, e) {
+          gameTitles.push($(e).html());
+        })
+        var gamePriceDecimal = []
+        $("#hola").find(".buy--price:parent").children(".decimal").each(function (i, e) {
+          gamePriceDecimal.push($(e).html());
+        });
+        var currency = $("#hola").find(".buy--price:parent").first().children(".currency").text();
+        var gamePrice = [];
+        $("#hola").find(".buy--price:parent").children(".int").each(function (i, e) {
+          gamePrice.push($(e).text() + gamePriceDecimal[i])
+        });
+        
+        var games = gameTitles.map(function (gameTitles, i) {
+          return {
+            'name': gameTitles,
+            'price': gamePrice[i],
+            'currency': currency
+          }
+        });
+        return games;
+      },
+    );
+  
+    $("#robar").on("click", function () { 
+      console.log(JSON.stringify(robar));
+     })
+  
+    $("#error").error(function() {
+      $("#error").show();
+    });
+  });
+  
--- javascript/scrapper.js
+++ javascript/scrapper.js
@@ -1,22 +1,38 @@
-$(document).ready(function (){
-    var gameTitles = []
-    $(".search-item:parent").children(".item-info").children('.title').children("a").each(function(i, e){
-      gameTitles.push($(e).text());
+$(document).ready(function () {
+  var robar = $.get("https://www.game.es/buscar/juegos-ps5", function (data) {
+    var newData = data.replace(/(\r\n|\n|\r|\t)/gm, "");
+    var newData2 = newData.substr(newData.indexOf('<body>'), newData.indexOf('</body>')+1)
+    $("#hola").html(newData2);
+      var gameTitles = []
+      $("#hola").find(".search-item:parent").children(".item-info").children('.title').children("a").each(function (i, e) {
+        gameTitles.push($(e).html());
       })
       var gamePriceDecimal = []
-      $(".buy--price:parent").children(".decimal").each(function(i, e) {
-        gamePriceDecimal.push($(e).text());
-      })
-      var currency = $(".buy--price:parent").first().children(".currency").text();
-      var gamePrice = []
-      $(".buy--price:parent").children(".int").each(function(i, e) {
-        gamePrice.push($(e).text() + gamePriceDecimal[i])
-      })
-      // Ya te liabas con TypeScript he pillin....
-      var games = gameTitles.map(function(gameTitles, i) {
-        return {'name':gameTitles,
-                'price':gamePrice[i],
-                'currency': currency}
+      $("#hola").find(".buy--price:parent").children(".decimal").each(function (i, e) {
+        gamePriceDecimal.push($(e).html());
       });
-      console.log(games)
+      var currency = $("#hola").find(".buy--price:parent").first().children(".currency").text();
+      var gamePrice = [];
+      $("#hola").find(".buy--price:parent").children(".int").each(function (i, e) {
+        gamePrice.push($(e).text() + gamePriceDecimal[i])
+      });
+      
+      var games = gameTitles.map(function (gameTitles, i) {
+        return {
+          'name': gameTitles,
+          'price': gamePrice[i],
+          'currency': currency
+        }
+      });
+      return games;
+    },
+  );
+
+  $("#robar").on("click", function () { 
+    console.log(JSON.stringify(robar));
+   })
+
+  $("#error").error(function() {
+    $("#error").show();
   });
+});
